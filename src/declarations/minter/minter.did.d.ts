bfa9626c976dccd7e0511755309ce2a94dc2dd8d
Isaac Valadez <hello@isaacvaladez.com>
2022-03-08 18:47:13 -0600

    added basic Plug integration

--- src/declarations/minter/minter.did.d.ts
+++ src/declarations/minter/minter.did.d.ts
@@ -3,6 +3,7 @@ export interface DRC721 {
   'approve' : (arg_0: Principal, arg_1: TokenId) => Promise<undefined>,
   'balanceOf' : (arg_0: Principal) => Promise<[] | [bigint]>,
   'getApproved' : (arg_0: bigint) => Promise<Principal>,
+  'http_request' : (arg_0: Request) => Promise<Response>,
   'isApprovedForAll' : (arg_0: Principal, arg_1: Principal) => Promise<boolean>,
   'mint' : (arg_0: string) => Promise<bigint>,
   'mint_principal' : (arg_0: string, arg_1: Principal) => Promise<bigint>,
@@ -19,5 +20,36 @@ export interface DRC721 {
       arg_2: bigint,
     ) => Promise<undefined>,
 }
+export type HeaderField = [string, string];
+export interface Request {
+  'url' : string,
+  'method' : string,
+  'body' : Array<number>,
+  'headers' : Array<HeaderField>,
+}
+export interface Response {
+  'body' : Array<number>,
+  'headers' : Array<HeaderField>,
+  'streaming_strategy' : [] | [StreamingStrategy],
+  'status_code' : number,
+}
+export type StreamingCallback = (arg_0: StreamingCallbackToken) => Promise<
+    StreamingCallbackResponse
+  >;
+export interface StreamingCallbackResponse {
+  'token' : [] | [StreamingCallbackToken],
+  'body' : Array<number>,
+}
+export interface StreamingCallbackToken {
+  'key' : string,
+  'index' : bigint,
+  'content_encoding' : string,
+}
+export type StreamingStrategy = {
+    'Callback' : {
+      'token' : StreamingCallbackToken,
+      'callback' : StreamingCallback,
+    }
+  };
 export type TokenId = bigint;
 export interface _SERVICE extends DRC721 {}
--- src/declarations/minter/minter.did.js
+++ src/declarations/minter/minter.did.js
@@ -1,9 +1,43 @@
 export const idlFactory = ({ IDL }) => {
   const TokenId = IDL.Nat;
+  const HeaderField = IDL.Tuple(IDL.Text, IDL.Text);
+  const Request = IDL.Record({
+    'url' : IDL.Text,
+    'method' : IDL.Text,
+    'body' : IDL.Vec(IDL.Nat8),
+    'headers' : IDL.Vec(HeaderField),
+  });
+  const StreamingCallbackToken = IDL.Record({
+    'key' : IDL.Text,
+    'index' : IDL.Nat,
+    'content_encoding' : IDL.Text,
+  });
+  const StreamingCallbackResponse = IDL.Record({
+    'token' : IDL.Opt(StreamingCallbackToken),
+    'body' : IDL.Vec(IDL.Nat8),
+  });
+  const StreamingCallback = IDL.Func(
+      [StreamingCallbackToken],
+      [StreamingCallbackResponse],
+      ['query'],
+    );
+  const StreamingStrategy = IDL.Variant({
+    'Callback' : IDL.Record({
+      'token' : StreamingCallbackToken,
+      'callback' : StreamingCallback,
+    }),
+  });
+  const Response = IDL.Record({
+    'body' : IDL.Vec(IDL.Nat8),
+    'headers' : IDL.Vec(HeaderField),
+    'streaming_strategy' : IDL.Opt(StreamingStrategy),
+    'status_code' : IDL.Nat16,
+  });
   const DRC721 = IDL.Service({
     'approve' : IDL.Func([IDL.Principal, TokenId], [], []),
     'balanceOf' : IDL.Func([IDL.Principal], [IDL.Opt(IDL.Nat)], []),
     'getApproved' : IDL.Func([IDL.Nat], [IDL.Principal], []),
+    'http_request' : IDL.Func([Request], [Response], ['query']),
     'isApprovedForAll' : IDL.Func(
         [IDL.Principal, IDL.Principal],
         [IDL.Bool],
